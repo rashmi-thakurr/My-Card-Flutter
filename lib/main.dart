ac3a9435eef0266f683ef7030de40b434f4ddc95
Rashmi Thakur <rashmi.lata151@gmail.com>
2021-12-13 13:27:11 +0530

    'my-card'

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,97 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    MyApp()
+  );
+}
+
+class  MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('Images/Portfolio-image.jpg'),
+              ),
+              Text(
+                'Rashmi Thakur',
+                style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
+                ),
+              ),
+              Text(
+                'STUDENT',
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.teal[100],
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Source Sans Pro',
+                  letterSpacing: 5,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      '+91 xxxx xxx xxx',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20,
+                      ),
+                    ) ,
+                  )
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+               child: Padding(
+                 padding: EdgeInsets.all(8.0),
+                 child: ListTile(
+                   leading: Icon(
+                     Icons.email,
+                     color: Colors.teal,
+                   ),
+                   title: Text(
+                     'rashmi.lata151@gmail.com',
+                     style: TextStyle(
+                       color: Colors.teal.shade900,
+                       fontFamily: 'Pacifico',
+                       fontSize: 20,
+                     ),
+                   ) ,
+                 )
+               ),
+              )
+            ],
+            ),
+
+        ),
       ),
-    ),
-  );
+      );
+  }
 }
